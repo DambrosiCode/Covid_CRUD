c48581465d81195126b50554212143d11fb6d334
DambrosiCode <matt.t.dambrosio@gmail.com>
2023-08-28 18:03:35 -0400

    Switch between datasets

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dash_table, dcc, html, Input, Output, State, callback
+from dash import Dash, dash_table, dcc, ctx, html, Input, Output, State, callback
 from pymongo_get_db import get_database
 from mongo_manip import id_df_change
 import pandas as pd
@@ -6,7 +6,6 @@ from bson import ObjectId
 from pymongo import UpdateOne, UpdateMany
 
 app = Dash(__name__)
-#TODO: Change between collections
 #TODO: Searchable
 #TODO: Don't allow editing of _id
 #TODO: include input limiting
@@ -20,12 +19,12 @@ app = Dash(__name__)
 #TODO: Vizualize changes that have been made before update
 
 
-
-#get data from mongoDB return as dataframe
+# get data from mongoDB return as dataframe
+#TODO: delete this and replace it
 mongo_db = get_database('Covid19_Risk_Factors')
 collection = mongo_db['Test']
 df = pd.DataFrame(collection.find())
-#covnvert object ID to string
+# covnvert object ID to string
 df['_id'] = df['_id'].astype(str)
 print('========')
 
@@ -38,10 +37,16 @@ app.layout = html.Div(children=[
         ],
         data=df.to_dict('records'),
         editable=True,
-        row_deletable=True
+        row_deletable=True,
+        filter_action='native',
+        sort_action='native',
+        page_action='native',
+        page_current=0,
+        page_size=10,
     ),
     html.Button('Add Row', id='add-row-button', n_clicks=0),
     html.Button('Update Database', id='mongo-update-button', n_clicks=0),
+    dcc.Dropdown(id='collection-dropdown', options=sorted(mongo_db.collection_names()), value='Test'),
 
     #confirmation popup
     dcc.ConfirmDialog(
@@ -50,19 +55,50 @@ app.layout = html.Div(children=[
     ),
 
     #store initial dataframe in dcc.Store to compare usermade changes later
-    dcc.Store(id='original-dataframe', data=df.to_dict("records"))
+    dcc.Store(id='original-dataframe', data=df.to_dict("records")),
+    #dcc.Store(id='collection-id', data='Test')
 ])
 
-#add new rows
-@callback(
-    Output('editable-table', 'data'),
-    Input('add-row-button', 'n_clicks'),
-    State('editable-table', 'data'),
-    State('editable-table', 'columns'))
-def add_row(n_clicks, rows, columns):
-    if n_clicks > 0:
+#edit dataframe
+@callback([Output('editable-table', 'data'),
+           Output('editable-table','columns'),
+           Output('editable-table', 'page_current'),],
+          Input('collection-dropdown', 'value'),
+          Input('add-row-button', 'n_clicks'),
+          Input('editable-table', 'data'),
+          State('editable-table', 'data'),
+          State('editable-table', 'columns'),
+          State('editable-table', 'page_size'))
+def build_dataframe(selected_collection, n_clicks, data_frame, rows, columns, page_size):
+    trigger = ctx.triggered_id
+
+
+    #get number of pages from pagination
+    last_page = len(data_frame) // page_size + (len(data_frame) % page_size > 0)
+
+    #append new rows if add rows is clicked
+    if trigger == 'add-row-button':
         rows.append({c['id']: str(ObjectId()) for c in columns})
-    return rows
+
+
+
+        return rows, None, last_page-1
+    #switch to a new dataframe
+    elif trigger == 'collection-dropdown':
+        # get data from mongoDB return as dataframe
+        mongo_db = get_database('Covid19_Risk_Factors')
+        collection = mongo_db[selected_collection]
+        df = pd.DataFrame(collection.find())
+        # covnvert object ID to string
+        df['_id'] = df['_id'].astype(str)
+
+        #get columns
+        columns=[
+            {'name': col, 'id': col, 'deletable':False} for col in df.columns
+        ]
+
+        return df.to_dict('records'), columns, 0
+
 
 #TODO: should new columns be addable?
 
