ad33129dd2ae60f4b415c8ba201578df9f2cfe67
DambrosiCode <matt.t.dambrosio@gmail.com>
2023-08-31 14:06:17 -0400

    csv upload

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 from dash import Dash, dash_table, dcc, ctx, html, Input, Output, State, callback
 from pymongo_get_db import get_database
-from mongo_manip import id_df_change
+from mongo_manip import id_df_change, data_validation
 import pandas as pd
 from bson import ObjectId
 from pymongo import UpdateOne, UpdateMany
+import io
+import base64
 
 app = Dash(__name__)
 # TODO: include input limiting
-# TODO: Upload CSV
 # TODO: Verify user when changes are made
-# TODO: Auto-scroll to end of page when Add Row is pressed
 # TODO: Vizualize statistics
 # TODO: Vizualize changes that have been made before update
 # TODO: refresh dataframe manually and/or intervally
@@ -47,20 +47,20 @@ class MongoDBConnector:
 mongo_connector = MongoDBConnector(default_collection_name='Age')
 # dataframe at startup
 mongo_initial_df = mongo_connector.get_mongo_as_df()
-target_cols = ['Date', 'Study', 'Study_Link', 'Journal','Sample_Size', 'Study_Type',
+target_cols = ['Date', 'Study', 'Study_Link', 'Journal', 'Sample_Size', 'Study_Type',
                'Severe', 'Fatality',
 
-               'Severe_lower_bound', 'Severe_upper_bound','Severe_p-value',
+               'Severe_lower_bound', 'Severe_upper_bound', 'Severe_p-value',
                'Severe_Adjusted', 'Severe_Calculated',
-               'Fatality_lower_bound',  'Fatality_upper_bound', 'Fatality_p-value',
+               'Fatality_lower_bound', 'Fatality_upper_bound', 'Fatality_p-value',
                'Fatality_Adjusted', 'Fatality_Calculated',
                'Multivariate_adjustment',
                'Critical_only', 'Discharged_vs._death?',
                '_id'
                ]
-mongo_initial_df = mongo_initial_df.loc[:,target_cols] #subset desired cols
+mongo_initial_df = mongo_initial_df.loc[:, target_cols]  # subset desired cols
 # collection cursor
-#mongo_collection_cursor = mongo_connector.get_mongo_collection()
+# mongo_collection_cursor = mongo_connector.get_mongo_collection()
 
 app.layout = html.Div(children=[
     # datatable
@@ -68,21 +68,21 @@ app.layout = html.Div(children=[
         id='editable-table',
         columns=[
             {'name': col, 'id': col, 'deletable': False, 'editable': (col != '_id')}
-            for col in mongo_initial_df.columns  # TODO: custom columns
+            for col in mongo_initial_df.columns
         ],
         style_table={
             'overflowX': 'auto',
             'maxHeight': '90vh',
             'overflowY': 'auto',  # Enable vertical scrolling
         },
-        style_cell={'minWidth':'150px',
-                    'maxWidth':'150px',
-                    'maxHeight':'auto',
-                    'textAlign':'left',
-                    'whiteSpace':'normal',
-                    'textOverflow':'ellipsis'},
+        style_cell={'minWidth': '150px',
+                    'maxWidth': '150px',
+                    'maxHeight': 'auto',
+                    'textAlign': 'left',
+                    'whiteSpace': 'normal',
+                    'textOverflow': 'ellipsis'},
 
-        fixed_rows={'headers':True},
+        fixed_rows={'headers': True},
         data=mongo_initial_df.to_dict('records'),
         editable=True,
         row_deletable=True,
@@ -90,9 +90,10 @@ app.layout = html.Div(children=[
         sort_action='native',
         page_action='native',
         page_current=0,
-        page_size=100,
+        page_size=5,
     ),
     html.Button('Add Row', id='add-row-button', n_clicks=0),
+    dcc.Upload(html.Button('Upload .csv'), id='csv-upload'),
     html.Button('Update Database', id='mongo-update-button', n_clicks=0),
     dcc.Dropdown(id='collection-dropdown', options=sorted(mongo_db.list_collection_names()), value='Age'),
 
@@ -102,10 +103,39 @@ app.layout = html.Div(children=[
         message='Updated Confirmed',
     ),
 
+    dcc.ConfirmDialog(
+        id='validation-error'
+    ),
+
     # store initial dataframe in dcc.Store to compare usermade changes later
     dcc.Store(id='old-dataframe', data=mongo_initial_df.to_dict("records")),
     # dcc.Store(id='collection-id', data='Test')
 ])
+
+
+# update user if validation fails
+@callback([Output('validation-error', 'message'),
+          Output('validation-error', 'displayed')],
+
+          Input('mongo-update-button', 'n_clicks'),
+
+          State('editable-table', 'data'),
+          State('old-dataframe', 'data'))
+def validate_notification(clicks, edited_df, old_df):
+    df_diff = id_df_change(old_df, edited_df)
+    new_entry_ids = [_id['_id'] for _id in df_diff]
+    edited_df = pd.DataFrame(edited_df)
+    new_entries = edited_df[edited_df['_id'].isin(new_entry_ids)]
+
+    validation = True
+
+    if validation and clicks > 0:
+        results = data_validation(new_entries)
+        print(results)
+        if results is not True:
+            return results, True
+        else:
+            return 'Validation Successful!', True
 
 
 # edit dataframe
@@ -113,28 +143,53 @@ app.layout = html.Div(children=[
            Output('editable-table', 'columns'),
            Output('editable-table', 'page_current'),
            Output('old-dataframe', 'data'),
-           Output('update-confirm-box', 'displayed'),],
+           Output('update-confirm-box', 'displayed'), ],
 
           Input('collection-dropdown', 'value'),
           Input('add-row-button', 'n_clicks'),
-          Input('editable-table', 'data'),
           Input('mongo-update-button', 'n_clicks'),
+          Input('csv-upload', 'contents'),
+          Input('csv-upload', 'filename'),
 
           State('editable-table', 'columns'),
           State('editable-table', 'page_size'),
           State('editable-table', 'data'),
           State('old-dataframe', 'data'))
-def build_dataframe(selected_collection, n_clicks, edited_data_frame, update_button,
+def build_dataframe(selected_collection, n_clicks, update_button, contents, filename,
                     columns, page_size, edited_df, old_df):
     # callback context
     trigger = ctx.triggered_id
 
+    # if user uploads a csv dataframe
+    if trigger == 'csv-upload':
+        # convert upload to dataframe
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+        csv_df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+
+        # remove _id column if applicable, then add new ObjectIds
+        csv_df = csv_df.drop(['_id'], axis=1, errors='ignore')
+        new_ids = [str(ObjectId()) for x in csv_df.index]
+
+        csv_df['_id'] = new_ids
+
+        # append dataframe to current collection
+        print(edited_df)
+        combined_df = pd.concat([pd.DataFrame(edited_df), csv_df], ignore_index=True)
+
+        print(combined_df)
+        last_page = (len(combined_df) + 1) // page_size + ((len(combined_df) + 1) % page_size > 0)
+
+        # return new dataframe as editable_df+csv_df, columns, go to last page of new df, don't update old df, no notifcation box
+        return combined_df.to_dict('records'), columns, last_page - 1, old_df, False
+
     # get number of pages from pagination
-    last_page = len(edited_data_frame) // page_size + (len(edited_data_frame) % page_size > 0)
+    last_page = (len(edited_df) + 1) // page_size + ((len(edited_df) + 1) % page_size > 0)
 
     # append new rows if add rows is clicked
     if trigger == 'add-row-button':
         edited_df.append({c['id']: str(ObjectId()) for c in columns})
+        print(last_page)
 
         # add new row to editable dataframe, update columns, set start page to last page, keep dcc.Store dataframe to unupdated fd
         return edited_df, columns, last_page - 1, old_df, False
@@ -153,9 +208,8 @@ def build_dataframe(selected_collection, n_clicks, edited_data_frame, update_but
     elif trigger == 'mongo-update-button':
         print('update button')
         print(selected_collection)
-        #set new cursor to selected collection
-        mongo_collection_cursor=mongo_connector.get_mongo_collection(selected_collection)
-
+        # set new cursor to selected collection
+        mongo_collection_cursor = mongo_connector.get_mongo_collection(selected_collection)
 
         if update_button > 0:
 
@@ -193,7 +247,7 @@ def build_dataframe(selected_collection, n_clicks, edited_data_frame, update_but
                 bulk_operations = []
                 for i, doc in enumerate(updated_rows):
                     bulk_operations.append(UpdateMany({'_id': ObjectId(doc['_id'])},
-                                                     {'$set': updated_rows_noid[i]}))
+                                                      {'$set': updated_rows_noid[i]}))
                 print(bulk_operations)
                 mongo_collection_cursor.bulk_write(bulk_operations)
 
--- mongo_manip.py
+++ mongo_manip.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import re
 def id_df_change(unedited_df, edited_df):
     changes = []
 
@@ -23,3 +23,44 @@ def id_df_change(unedited_df, edited_df):
 
     return changes
 
+
+# Ensure that data is in a valid format for each row before uploading
+def data_validation(df):
+    errors = []
+
+    # validation schema
+    date_r = r'^\d{4}-\d{2}-\d{2}$'
+    link_r = r'^http.*$'
+    Sample_Size_r = int
+    Critical_only_r = ['Y', 'N', '']
+    Discharged_vs_death_r = ['Y', 'N', '']
+
+    for index in df['_id']:
+        print(index)
+        row = df[df['_id']==index]
+        print(row.Date.astype("string"))
+        # validate date
+        if re.match(date_r, str(row['Date'])) is None:
+            errors.append('index: ' + str(index) + " Date: YYYY-MM-dd")
+            print('123123123')
+        # validate Study Link
+        if re.match(link_r, str(row.Study_Link)) is None:
+            errors.append('index: ' + str(index) + ' Study Link: http(s) link')
+
+            # validate Sample Size
+        if str(row.Sample_Size) is not Sample_Size_r:
+            errors.append('index: ' + str(index) + ' Sample Size: int')
+
+
+            # validate Critical Only
+        if str(row.Critical_only) not in Critical_only_r:
+            errors.append('index: ' + str(index) + ' Critical Only: "Y/N/''"')
+
+            # validate Discharged vs death
+        if str(row['Discharged_vs._death?']) not in Discharged_vs_death_r:
+            errors.append('index: ' + str(index) + ' Discharged_vs._death?: "Y/N/''"')
+
+    if len(errors) > 0:
+        return "\n".join(errors)
+    else:
+        return True
