3454d6dd0ce86933d50b2f47365403318e859f6c
Matthew D'Ambrosio <48102443+DambrosiCode@users.noreply.github.com>
2023-09-06 17:17:23 -0400

    Update pymongo_insert.py

--- pymongo_insert.py
+++ pymongo_insert.py
@@ -23,3 +23,11 @@ def upload_csv(db_name, collection_name, csv_path):
 
 
 
+if __name__ == "__main__":
+    # Specify your database name, collection name, and CSV file path here
+    database_name = "your_database_name"
+    collection_name = "your_collection_name"
+    csv_file_path = "your_csv_file.csv"
+    
+    # Call the function to upload the CSV data to MongoDB
+    upload_csv_to_mongodb(database_name, collection_name, csv_file_path)
